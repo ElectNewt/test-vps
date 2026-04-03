a93805bf07953bc462faee69901bb801b38a6b8c
Netmentor <ivanxo69@gmail.com>
2026-04-03 14:05:45 +0100

    add test project

---
+++ BlazorApp1.Tests/Components/Pages/CounterTests.cs
@@ -0,0 +1,15 @@
+﻿using BlazorApp1.Components.Pages;
+using Bunit;
+
+namespace BlazorApp1.Tests.Components.Pages;
+
+public class CounterTests : BunitContext
+{
+    [Fact]
+    public void WhenButtonIsClicked_ThenCounterShouldIncrement()
+    {
+        var cut = Render<Counter>();
+        cut.Find("button").Click();
+        cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 1</p>");
+    }
+}
